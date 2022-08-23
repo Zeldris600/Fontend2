4752c5018dbbd82269a0fcb5d15e0a95c42b63c7
Joram <abayojoram696@gmail.com>
2022-08-23 22:46:08 +0100

    Implemented a class as per assignment

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,52 @@
-let counter = 60;
-let body1 = document.getElementById('cont');
+// let counter = 60;
 
-  let b = setInterval((
+class UI {
+  constructor(counter) {
+    this.counter = counter;
+  }
 
-)=>{
+  change() {
+    let b = setInterval(() => {
+      let body1 = document.getElementById("cont");
+      let counter = this.counter;
+      if (counter > 0) {
+        this.counter -= 5;
+        switch (true) {
+          case counter % 2 == 0 && !counter % 5 == 0:
+            body1.style.cssText = `
+                     background-color: hsl(45, 94%, 49%)`; //yellow
+            break;
+          case counter % 5 == 0 && counter > 10:
+            body1.style.cssText = `
+                     background-color:  hsl(114, 92%, 9%)`; //green
+            break;
+          case counter < 10:
+            body1.style.cssText = `
+                      background-color: hsl(6, 94%, 69%)`; //red
+        }
+      } else {
+        body1.style.backgroundColor = "hsl(0, 100%, 74%)";
+        clearTimeout(b);
+      }
 
-    if(counter > 0){
-        counter-=5;
-        /* if(counter % 2 == 0){
-          body1.style.cssText = `
-          background-color: hsl(140, 22%, 64%);
-          
-          `
-        } */
-      
-        switch (counter > 0) {
-           case (counter % 2 == 0 && !counter % 5 == 0):
-              body1.style.cssText = `
-             background-color: hsl(45, 94%, 49%)`//yellow
-              break; 
-           case (counter % 5 == 0 && counter > 10 ):
-              body1.style.cssText = `
-             background-color:  hsl(114, 92%, 9%)`//green
-              break; 
-           case (counter < 10 ):
-              body1.style.cssText = `
-              background-color: hsl(6, 94%, 69%)` //red
-              
-          }
-    } else 
-    
-    
-    {
-        body1.style.backgroundColor = 'hsl(0, 100%, 74%)'
-        clearTimeout(b)};
+      console.log(counter);
+      let displayCounter = document.getElementById('displayCounter');
+      displayCounter.innerHTML = `Counter is at ${this.counter}`
+    }, 1000);
+  }
+}
 
-console.log(counter);
 
-},5000)
+
+const myUI = new UI(60);
+const myUI2 = new UI(100);
+
+let userCounter = document.getElementById('inputCounter');
+
+function changer(){
+let userCounter3 = userCounter.value
+ let userCounter2 = new UI(userCounter3);
+ userCounter2.change()
+ userCounter.value = '';
+ 
+}
